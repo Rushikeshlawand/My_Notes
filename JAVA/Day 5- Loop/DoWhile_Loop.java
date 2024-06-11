ca1778f81ee29627833affa893c31bfd132f31d9
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-06-11 13:29:02 +0530

    11-6

---
+++ JAVA/Day 5- Loop/DoWhile_Loop.java
@@ -0,0 +1,11 @@
+public class DoWhile_Loop {
+
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<10);
+    }
+}
---
+++ JAVA/Day 5- Loop/F0r_Loop.java
@@ -0,0 +1,7 @@
+public class F0r_Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
