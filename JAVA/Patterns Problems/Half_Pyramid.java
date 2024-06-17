3fd9b9194c4b2ac43422c581fc0d28b8f8346b53
Rushikesh Shrimant Lawand <103987701+Rushikeshlawand@users.noreply.github.com>
2024-06-17 06:53:30 +0000

    17-6

--- JAVA/Patterns Problems/Half_Pyramid.java
+++ JAVA/Patterns Problems/Half_Pyramid.java
@@ -1,9 +1,9 @@
 public class Half_Pyramid {
-    public static void main(String args[]){
-        int n=10;
+    public static void main(String args[]) {
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) { // number of i star can only print on column. when number of i increases then
+                                           // number of * print accordingly.
                 System.out.print("*");
             }
             System.out.println();
