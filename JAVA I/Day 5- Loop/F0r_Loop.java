a50f0ec9eb257aac803dacbec178a05764b51782
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-06-22 10:49:18 +0530

    21-6

--- JAVA I/Day 5- Loop/F0r_Loop.java
+++ JAVA I/Day 5- Loop/F0r_Loop.java
@@ -1,10 +1,12 @@
 //even or odd
 public class F0r_Loop {
     public static void main(String[] args) {
-        for (int i = 200; i <= 500; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i);
+        for (int i = 1; i <= 5; i++) {
+            for (int j=1;j<=4;j++){
+                System.out.print(i+" "+j);
             }
+            System.out.println();
+
         }
     }
 }
--- JAVA I/Patterns Problems/solid_rectangle.java
+++ JAVA I/Patterns Problems/solid_rectangle.java
@@ -1,10 +1,9 @@
 public class solid_rectangle {
     public static void main(String args[]){
-        int n=5;
-        int m=5;
-        for (int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-        System.out.print("*");
+
+        for (int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+        System.out.print(i);
     }
         System.out.println();
 }
