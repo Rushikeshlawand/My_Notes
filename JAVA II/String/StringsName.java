d686a4d1bb57445f78692c4c3007d6bfb9145bf9
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-07-08 18:44:50 +0530

    8-7

---
+++ JAVA II/String/StringsName.java
@@ -0,0 +1,9 @@
+public class StringsName{
+    public static void main(String[] args) {
+        String Name="Rushikesh";
+        String Name1=new String("Lawand");
+
+        System.out.println(Name+" "+Name1);
+        System.out.println(Name.concat(Name1));
+    }
+}
