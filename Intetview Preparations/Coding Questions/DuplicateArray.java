6e7a39755f57e968186d90a898dfad3efd0c1acc
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-07-12 11:31:29 +0530

    12-7

--- Intetview Preparations/Coding Questions/DuplicateArray.java
+++ Intetview Preparations/Coding Questions/DuplicateArray.java
@@ -1,15 +1,33 @@
+import java.util.Arrays;
+
 public class duplicateArray {
     public static void main(String[] args) {
-        int[]array={1,1,1,2,2,3};
+        int[] array = { 1,2,3,4,4,5,5,5,6,7,0 };
         int n = array.length;
+        System.out.println("Element : Count");
 
-        for (int i = 0; i < n - 1; i++) {
-            if (array[i] == array[i + 1]) {
-                System.out.print(array[i] + " ");
-                while (i < array.length - 1 && array[i] == array[i + 1]) {
-                    i++;
-                }
+        int[]tempArray=new int[n];
+        int  newArrayIndex=0;
+        for (int i = 0; i < n;i++) {
+            int count = 1;
+            while (i < n - 1 && array[i] == array[i + 1]) {
+                count++;
+                i++;
             }
+            if (count > 1) {
+                System.out.println(array[i] + " : " + count);
+            }
+            tempArray[newArrayIndex++]=array[i];
+        }
+        int[]uniqueArray=Arrays.copyOf(tempArray, newArrayIndex);
+        System.out.print("Array without duplicates: ");
+        for(int i=0;i<uniqueArray.length;i++){
+            System.out.print(uniqueArray[i]+" ");
         }
     }
 }
+
+//Element : Count
+//4 : 2
+//5 : 3
+//Array without duplicates: 1 2 3 4 5 6 7 0
