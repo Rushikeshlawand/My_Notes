1567c40df75a62ca7a50882ae8860149ae727ace
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-07-15 19:21:15 +0530

    15-7

--- DSA I/Day 1- Linked List/linkedList.java
+++ DSA I/Day 1- Linked List/linkedList.java
@@ -10,6 +10,7 @@ class node {
 
 public class linkedList {
     node head;
+
     linkedList() {
         head = null;
     }
@@ -18,11 +19,32 @@ public class linkedList {
         node newNode = new node(data);
         if (head == null) {
             head = newNode;
+            return;
+        }
+        node last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = newNode;
+    }
+
+    void printData() {
+        node last = head;
+        while (last != null) {
+            System.out.println(last.data);
+            last = last.next;
+
         }
     }
+
     public static void main(String[] args) {
         linkedList obj = new linkedList();
         obj.insertAtTheEnd(10);
-        
+        obj.insertAtTheEnd(20);
+        obj.insertAtTheEnd(30);
+        obj.insertAtTheEnd(40);
+        obj.insertAtTheEnd(50);
+        obj.insertAtTheEnd(60);
+        obj.printData();
     }
 }
