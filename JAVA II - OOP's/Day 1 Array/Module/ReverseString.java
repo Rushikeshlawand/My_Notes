1ad3738751ad9fadcdeb7f19bac5590e7de1dcdc
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-07-24 12:48:54 +0530

    24-7

--- JAVA II - OOP's/Day 1 Array/Module/ReverseString.java
+++ JAVA II - OOP's/Day 1 Array/Module/ReverseString.java
@@ -2,7 +2,7 @@ package Module;
 public class ReverseString {
     public static void main(String[] args) {
 
-        String name = "Rushikesh", Reversed = " ";
+        String name = "Rushikesh", Reversed = "";
         char ch;
         for (int i = 0; i < name.length(); i++) {
             ch = name.charAt(i);
