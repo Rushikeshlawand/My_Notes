f33b75c5a1b4da67c76fcc3969ae0136fbe53feb
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-07-24 13:26:38 +0530

    24-7

--- JAVA II - OOP's/Day 1 Array/ReverseString.java
+++ JAVA II - OOP's/Day 1 Array/ReverseString.java
@@ -1,9 +1,9 @@
 public class ReverseString {
     public static void main(String str[]) {
-        String revv = "hiii";
+        String name = "RushikeshLawand";
         String rev = "";
-        for (int i = revv.length() - 1; i >= 0; i--) {
-            rev += revv.charAt(i);
+        for (int i = name.length() - 1; i >= 0; i--) {
+            rev += name.charAt(i);
         }
         System.out.println("Reversed string: " + rev);
     }
--- JAVA II - OOP's/Day 1 Array/arrayAscendingOrder.java
+++ JAVA II - OOP's/Day 1 Array/arrayAscendingOrder.java
@@ -6,7 +6,7 @@ public class arrayAscendingOrder {
 
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (array[i] < array[j]) {  //for descending just changes the oper
+                if (array[i] < array[j]) { // for descending just changes the oper
                     temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
