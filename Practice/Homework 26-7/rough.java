917df8b1dbc1814a0a1f7055ef59c151622b4e33
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-07-31 23:26:19 +0530

    31-7

--- Practice/Homework 26-7/rough.java
+++ Practice/Homework 26-7/rough.java
@@ -1,21 +1,10 @@
 public class rough {
-
     public static void main(String[] args) {
-        int[] prices = { 7, 1, 5, 3, 6, 4 };
-        int buy = prices[0], sell = prices[0];
-        for (int i = 0; i < prices.length; i++) {
-
-            if (prices[i] < buy) {
-                buy = prices[i];
-            }
-            if (prices[i] > sell) {
-                sell = prices[i];
-            }
-
+        int[] array = { 1, 2, 3 };
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum+array[i];
         }
-
-        System.out.println(sell);
-        System.out.println(buy);
-
+        System.out.print(sum);
     }
 }
