83c4dc92398162273b3e9266b8b71b5b3adf9b82
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-08-01 18:28:55 +0530

    1-8

---
+++ DSA I/Topic 3 - HashMap/k.java
@@ -0,0 +1,14 @@
+import java.util.Hashtable;
+
+public class k {
+
+    public static void main(String[] args) {
+        Hashtable<Integer, String> tb = new Hashtable<>();
+
+        tb.put(10, "Ram");
+        tb.put(11, "Ram");
+        tb.put(12, "Ram");
+        tb.put(13, "Ram");
+        System.out.println(tb);
+    }
+}
