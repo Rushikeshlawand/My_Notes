84be266180ed6d19489279f74188d52bb826a057
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-08-07 17:04:05 +0530

    7-8

--- DSA I/Topic 1- Linked List/addElementInSpecificPos.java
+++ DSA I/Topic 1- Linked List/addElementInSpecificPos.java
@@ -1,61 +1,61 @@
-class node {
-    int data;
-    node next;
+// class node {
+//     int data;
+//     node next;
 
-    node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
+//     node(int data) {
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
 
-public class addElementInSpecificPos {
-    node head;
+// public class addElementInSpecificPos {
+//     node head;
 
-    addElementInSpecificPos() {
-        head = null;
-    }
+//     addElementInSpecificPos() {
+//         head = null;
+//     }
 
-    void insertAtAnypoint(node pre, int data) {
-        if (pre.next == null) {
-            return;
-        }
-        node newnNode = new node(data);
-        newnNode.next = pre.next;
-        pre.next = newnNode;
-    }
+//     void insertAtAnypoint(node pre, int data) {
+//         if (pre.next == null) {
+//             return;
+//         }
+//         node newnNode = new node(data);
+//         newnNode.next = pre.next;
+//         pre.next = newnNode;
+//     }
 
-    void insertAtTheEnd(int data) {
-        node newNode = new node(data);
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        node last = head;
-        while (last.next != null) {
-            last = last.next;
-        }
-        last.next = newNode;
-    }
+//     void insertAtTheEnd(int data) {
+//         node newNode = new node(data);
+//         if (head == null) {
+//             head = newNode;
+//             return;
+//         }
+//         node last = head;
+//         while (last.next != null) {
+//             last = last.next;
+//         }
+//         last.next = newNode;
+//     }
 
-    void printData() {
-        node last = head;
-        while (last != null) {
-            System.out.println(last.data);
-            last = last.next;
+//     void printData() {
+//         node last = head;
+//         while (last != null) {
+//             System.out.println(last.data);
+//             last = last.next;
 
-        }
-    }
+//         }
+//     }
 
-    public static void main(String[] args) {
-        addElementInSpecificPos obj = new addElementInSpecificPos();
-        obj.insertAtTheEnd(10);
-        obj.insertAtTheEnd(20);
-        obj.insertAtTheEnd(30);
-        obj.insertAtTheEnd(40);
-        obj.insertAtTheEnd(50);
+//     public static void main(String[] args) {
+//         addElementInSpecificPos obj = new addElementInSpecificPos();
+//         obj.insertAtTheEnd(10);
+//         obj.insertAtTheEnd(20);
+//         obj.insertAtTheEnd(30);
+//         obj.insertAtTheEnd(40);
+//         obj.insertAtTheEnd(50);
 
-        node addNode=obj.head.next.next.next;
-        obj.insertAtAnypoint(addNode, 1);
-        obj.printData();
-    }
-}
+//         node addNode=obj.head.next.next.next;
+//         obj.insertAtAnypoint(addNode, 1);
+//         obj.printData();
+//     }
+// }
