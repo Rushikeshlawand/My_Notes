da197e2f5a34c0916b03a72742ac2629d6b52e61
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-08-12 10:01:50 +0530

    12-8

--- DSA I/Topic 5-  Tree/sumTree.java
+++ DSA I/Topic 5-  Tree/sumTree.java
@@ -1,56 +1,47 @@
-import java.util.LinkedList;
-import java.util.Queue;
+// import java.util.LinkedList;
+// import java.util.Queue;
 
-class Node {
-    int data;
-    Node left;
-    Node right;
+// class Node {
+//     int data;
+//     Node left;
+//     Node right;
 
-    Node(int data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
-}
+//     Node(int data) {
+//         this.data = data;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
 
-public class sumTree {
+// public class sumTree {
 
-    static void InsertData(Node root, int Key) {
-        Queue<Node> qt = new LinkedList<>();
-        qt.add(root);
-        while (!qt.isEmpty()) {
-            Node temp = qt.poll();
-            if (temp.left == null) {
-                temp.left = new Node(Key);
-                break;
-            } else {
-                qt.add(temp.left);
-            }
-            if (temp.right == null) {
-                temp.right = new Node(Key);
-                break;
-            } else {
-                qt.add(temp.right);
-            }
-        }
-    }
 
-    static boolean sumTree(Node root) {
-        if (root == null) {
-            return false;
-        }
+
+//     static int sum(Node dt) {
+//         if (dt == null) {
+//             return 0;
+//             return dt.data + sum(dt.left) + sum(dt.right);
+//         }
+
+//     }
+
+//     static boolean isSunTree(Node dt){
         
+//         if(dt==null || (dt.left==null && dt.right==null))
+//         return true;
+//         int lTreeSum=Sum(dt.left);
+//         int rTreeSum-Sum(dt.right) ;
+//         Af(dt.data==lTreeSum+rTreeSun && isSumTree(dt.left) 66 isSunTree(dt.right))
+//         return true;
+//         return false;
+//         }
+//     public static void main(String[] args) {
+//         Node root = new Node(1);
+//         root.left = new Node(2);
+//         root.right = new Node(3);
+//         root.left.right = new Node(4);
+//         root.right.left = new Node(5);
+//         root.left.left = new Node(6);
 
-        return false;
-    }
-
-    public static void main(String[] args) {
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.right = new Node(4);
-        root.right.left = new Node(5);
-        root.left.left = new Node(6);
-
-    }
-}
+//     }
+// }
--- DSA I/Topic 5-  Tree/treeStructure.java
+++ DSA I/Topic 5-  Tree/treeStructure.java
@@ -1,59 +1,59 @@
-// class Node {
-//     int data;
-//     Node left;
-//     Node right;
+class Node {
+    int data;
+    Node left;
+    Node right;
 
-//     Node(int data) {
-//         this.data = data;
-//         this.left = null;
-//         this.right = null;
-//     }
-// }
-// public class treeStructure {
-//     static void preorder(Node root) {
-//         if (root == null) {
-//             return;
-//         }
-//         System.out.println(root.data);
-//         preorder(root.left);
-//         preorder(root.right);
-//     }
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+public class treeStructure {
+    static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.data);
+        preorder(root.left);
+        preorder(root.right);
+    }
 
-//     static void postorder(Node root) {
-//         if (root == null) {
-//             return;
-//         }
-//         postorder(root.left);
-//         postorder(root.right);
-//         System.out.println(root.data);
-//     }
+    static void postorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.println(root.data);
+    }
 
-//     static void inorder(Node root) {
-//         if (root == null) {
-//             return;
-//         }
-//         inorder(root.left);
-//         System.out.println(root.data);
-//         inorder(root.right);
-//     }
+    static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data);
+        inorder(root.right);
+    }
 
-//     public static void main(String[] args) {
-//         Node root = new Node(1);
-//         root.left = new Node(2);
-//         root.right = new Node(3);
-//         root.left.right = new Node(4);
-//         root.right.left = new Node(5);
-//         root.left.left = new Node(6);
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.right = new Node(4);
+        root.right.left = new Node(5);
+        root.left.left = new Node(6);
 
-//         System.out.println("Preorder traversal:");
-//         preorder(root);
-//         System.out.println();
+        System.out.println("Preorder traversal:");
+        preorder(root);
+        System.out.println();
 
-//         System.out.println("Inorder traversal:");
-//         inorder(root);
-//         System.out.println();
+        System.out.println("Inorder traversal:");
+        inorder(root);
+        System.out.println();
 
-//         System.out.println("Postorder traversal:");
-//         postorder(root);
-//     }
-// }
+        System.out.println("Postorder traversal:");
+        postorder(root);
+    }
+}
