f34004dfdd85d65f045a2bfc2edf290f862f3010
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-08-22 15:13:44 +0530

    22-8

--- DSA II - Algorithms/Topic 1 - Sorting/Insertionsort.java
+++ DSA II - Algorithms/Topic 1 - Sorting/Insertionsort.java
@@ -1,19 +1,36 @@
 public class Insertionsort {
-    public static void main(String[] args) {
-        int[] array = { 4, 55, -22, 26 };
 
-        for (int i = 1; i < array.length; i++) {
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
             int key = array[i];
             int j = i - 1;
+
             while (j >= 0 && array[j] > key) {
                 array[j + 1] = array[j];
                 j = j - 1;
             }
             array[j + 1] = key;
         }
+    }
 
-        for (int i = 1; i < array.length; i++) {
+    public static void printArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n; ++i) {
             System.out.print(array[i] + " ");
         }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 12, 11, 13, 5, 6 };
+
+        System.out.println("Original Array:");
+        printArray(array);
+
+        insertionSort(array);
+
+        System.out.println("Sorted Array:");
+        printArray(array);
     }
 }
---
+++ DSA II - Algorithms/Topic 1 - Sorting/mergeSort.java
@@ -0,0 +1,20 @@
+public class mergeSort {
+    public static void merge(int[] array, int l, int r) {
+        for (int i = 0; i < array.length; i++) {
+            int mid = (l + r) / 2;
+            
+        }
+    }
+
+    public static void print(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 235, 57, 33, 99, -11 };
+        int l=array[0];
+        int r=array[array.length-1];
+    }
+}
